2643afdaa4f91eed4c18ed9e22baa9f957324a5f
patan naziya <patannaziyatasleem@gmail.com>
2024-09-12 16:25:50 +0530

    added terraform

--- expense/outputs.tf
+++ expense/outputs.tf
@@ -1,3 +1,6 @@
+#use output.instances_info
+#output.instances_info ==  aws_instance.expense  // this way we can directly call the output or below like also we can do.
+
 output "instances_info" {
     value = aws_instance.expense
 }
